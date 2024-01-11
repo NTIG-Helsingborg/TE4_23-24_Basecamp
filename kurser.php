52de5ff9e2c1f586437a744ee15a608b615eb2a8
Salah.Alhassun <142593904+Salah9320Alhassun@users.noreply.github.com>
2024-01-11 09:43:05 +0100

    Skapar filer för kurserna php o css

---
+++ kurser.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Kurser</title>
+        <link rel="stylesheet" href="./kurser.css">
+</head>
+<body>
+        <h1>Välkommen till kurserna</h1>
+        
+        
+</body>
+</html>
