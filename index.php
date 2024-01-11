267a7f6eeabf9d574c51bffa88db82d11f94f372
zimonjohnsson <zimonjohnsson2004@gmail.com>
2024-01-11 12:02:45 +0100

    make navbar same on all pages

--- index.php
+++ index.php
@@ -36,60 +36,60 @@ session_Start();
 	<header>
 		<!-- NAVBAR -->
 		<nav class="navbar navbar-expand-md navbarBG navbar-dark fixed-top">
-			<a class="navbar-brand" href="#">
-				<img src="Images/Base_Camp_3.0.png" alt="Logo" style="width: 90px;">
-				BaseCamp
-			</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
-				<ul class="navbar-nav ml-auto ">
+		<a class="navbar-brand" href="index.php">
+			<img src="Images/Base_Camp_3.0.png" alt="Logo" style="width: 90px;">
+                        BaseCamp
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="collapsibleNavbar">
+			<ul class="navbar-nav navbarmiddle">
 
-					<!-- Navbar Dropdown -->
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-							Kurser
-						</a>
-						<div class="dropdown-menu">
-							<ul>
-								<li><a class="dropdown-item" href="#">Programmering 1</a></li>
-								<li><a class="dropdown-item" href="#">Programmering 2</a></li>
+				<!-- Navbar Dropdown -->
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+						Kurser
+					</a>
+					<div class="dropdown-menu">
+						<ul>
+							<li><a class="dropdown-item" href="#">Programmering 1</a></li>
+							<li><a class="dropdown-item" href="#">Programmering 2</a></li>
 
-								<li><a class="dropdown-item" href="#">Webbutveckling 1</a></li>
-								<li><a class="dropdown-item" href="#">Webbutveckling 2</a></li>
+							<li><a class="dropdown-item" href="#">Webbutveckling 1</a></li>
+							<li><a class="dropdown-item" href="#">Webbutveckling 2</a></li>
 
-								<li><a class="dropdown-item" href="#">Webbserver utveckling 1</a></li>
-								<li><a class="dropdown-item" href="#">Programmering 1</a></li>
+							<li><a class="dropdown-item" href="#">Webbserver utveckling 1</a></li>
+							<li><a class="dropdown-item" href="#">Programmering 1</a></li>
 
-							</ul>
-						</div>
-					</li>
+						</ul>
+					</div>
+				</li>
 
 					<!-- NAVBAR SIGN UP and LOG IN -->
-					<?php
-					if (isset($_SESSION["userNAME"])) {
-						echo '
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">',
-							$_SESSION["userNAME"], '
-						</a>
-					
-						<div class="dropdown-menu">
-				  			<a class="dropdown-item" href="#">Profil</a>
-				  			<a class="dropdown-item" href="#">Inställningar</a>
-							<a class="dropdown-item" href="logout.php">Logga ut</a>
-						</div>
-			  		</li>
+				<?php
+				if (isset($_SESSION["userNAME"])) {
+					echo '
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">',
+						$_SESSION["userNAME"], '
+					</a>
+				
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="#">Profil</a>
+						<a class="dropdown-item" href="#">Inställningar</a>
+						<a class="dropdown-item" href="logout.php">Logga ut</a>
+					</div>
+			  	</li>
 				';
-					} else {
-						include('signupAndLogin.php');
-					}
-					?>
+				} else {
+					include('signupAndLogin.php');
+				}
+				?>
 
-				</ul>
-			</div>
-		</nav>
+			</ul>
+		</div>
+	</nav>
 
 
 		<!-- AUTOMATIC VIDEO BACKGROUND -->
