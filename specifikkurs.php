05e1660451538f7eb4f425bdd47d4fbf58f8533f
zimonjohnsson <zimonjohnsson2004@gmail.com>
2024-01-12 13:54:46 +0100

    fix sidebar

--- specifikkurs.php
+++ specifikkurs.php
@@ -50,6 +50,7 @@
             </ul>
         </div>
     </div>
+    <button onClick="ShowSideBar()" class="showsideBtn">></button>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
 		integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
 		crossorigin="anonymous"></script>
@@ -62,7 +63,12 @@
                 $(this).prev().find('.showlinks').html('&#11167;');
             });
         });
-    </script>
+        </script>
+        <script>
+            function ShowSideBar(){
+                document.getElementById("sidebar").classList.toggle("showsidebar");
+            }
+        </script>
 </body>
 
 </html>
