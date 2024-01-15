a3427c948ffb8304d276ef7615e4d9ae19d411e9
TheAzzieDev <asrielrosemary@gmail.com>
2024-01-15 08:45:45 +0100

    starting to work on admin login

--- getLoginInfo.php
+++ getLoginInfo.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <?php
-include('db.php'); //Create connection to databse.
-session_Start();
+    include('db.php'); //Create connection to databse.
+    session_Start();
+
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["Login"])){
+        $username = $_POST["emailL"];
+        $password = $_POST["passwordL"];
+        //$statement = "SELECT username FROM use";
+    }
 ?>
 <html>
 
 <body>
     <br />
-
+    sdf
     
 
 </body>
