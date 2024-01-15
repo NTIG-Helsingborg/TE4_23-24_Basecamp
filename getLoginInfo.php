43e9efcfe43a6759b1f008d41201c1c7ef8a8440
TheAzzieDev <asrielrosemary@gmail.com>
2024-01-15 08:54:14 +0100

    hejhej

--- getLoginInfo.php
+++ getLoginInfo.php
@@ -6,7 +6,9 @@
     if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["Login"])){
         $username = $_POST["emailL"];
         $password = $_POST["passwordL"];
-        //$statement = "SELECT username FROM use";
+        $statement = "SELECT id FROM users WHERE username = :username";
+        $passwordStatement = "SELECT password_hash FROM users WHERE id = :id";
+        $adminCheck = $db->exec("SELECT id FROM users WHERE username = 'Admin'");
     }
 ?>
 <html>
